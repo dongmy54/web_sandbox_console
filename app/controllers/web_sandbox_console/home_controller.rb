56463178ad054db2052ef270a5af782766d4f39a
dongmingyan <dongmingyan01@gmail.com>
2020-04-20 20:47:08 +0800

    add home controller

---
+++ app/controllers/web_sandbox_console/home_controller.rb
@@ -0,0 +1,6 @@
+require_dependency "web_sandbox_console/application_controller"
+
+module WebSandboxConsole
+  class HomeController < ApplicationController
+  end
+end
---
+++ app/helpers/web_sandbox_console/home_helper.rb
@@ -0,0 +1,4 @@
+module WebSandboxConsole
+  module HomeHelper
+  end
+end
