8af161e8b10ed2baab1e9f125d98c904175f2804
dongmingyan <dongmingyan01@gmail.com>
2020-04-21 09:44:54 +0800

    add http_basic_auth

--- lib/web_sandbox_console/configuration.rb
+++ lib/web_sandbox_console/configuration.rb
@@ -1,6 +1,8 @@
 module WebSandboxConsole
   # ip 白名单
   mattr_accessor :ip_whitelist
+  # 基本认证
+  mattr_accessor :http_basic_auth
   # 常量 黑名单 数组
   mattr_accessor :constant_blacklist
   # 类方法 黑名单 hash
@@ -9,6 +11,9 @@ module WebSandboxConsole
   mattr_accessor :instance_method_blacklist
   # 日志路径
   mattr_accessor :console_log_path
+
+  # 默认设置
+  @@http_basic_auth = {name: 'dmy', password: '123456'}
 
   # 内置 实例方法 黑名单
   INSTANT_METOD_BUILT_IN_BLACKLIST = {
